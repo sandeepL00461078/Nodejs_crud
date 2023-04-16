840418339e10de036ee6d9bc6b209cbcb6d0372f
Sandeep Polisetti <sunny.polisetty@gmail.com>
2023-04-15 19:49:28 -0500

    node js project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')  // express service rfequired for this app
+const app = express()  // start or initialize the express framework 
+const mongoose = require('mongoose') // require mongoose -- in terminal > mongod
+const url = 'mongodb://localhost:27017'
+
+
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+const con = mongoose.connection
+
+con.on('open', function(){  // this will give the messge when opened and connected
+    console.log('connected...')
+})
+
