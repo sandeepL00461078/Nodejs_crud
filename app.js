c79a04d7f985ce398d53e977ba1b53a01ad401f4
Sandeep Polisetti <sunny.polisetty@gmail.com>
2023-04-20 11:33:47 -0500

    Chnge the console

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')  // express service rfequired for this app
 const app = express()  // start or initialize the express framework 
 const mongoose = require('mongoose') // require mongoose -- in terminal > mongod
 const url = 'mongodb://localhost:27017'
-
+// Done some changes
 
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -10,5 +10,6 @@ const con = mongoose.connection
 
 con.on('open', function(){  // this will give the messge when opened and connected
     console.log('connected...')
+    console.log("chnaged")
 })
 
